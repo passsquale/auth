2757b9482eea5a55e05251a151534282030825dc
Evgeniy Roslyackov <eveniy.roslyackov@gmail.com>
2024-01-08 11:49:12 +0300

    Add Get method for test

--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/brianvoe/gofakeit"
+	desc "github.com/passsquale/auth/pkg/user_v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"log"
+	"net"
+)
 
+const grpcPort = 50051
+
+type server struct {
+	desc.UnimplementedUserV1Server
+}
+
+func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+	log.Printf("User id: %d", req.GetId())
+	return &desc.GetResponse{
+		Id: req.GetId(),
+		Info: &desc.UserInfo{
+			Name:  gofakeit.Name(),
+			Email: gofakeit.Email(),
+			Role:  desc.Role_ADMIN,
+		},
+		CreatedAt: timestamppb.New(gofakeit.Date()),
+		UpdatedAt: timestamppb.New(gofakeit.Date()),
+	}, nil
+}
 func main() {
-	fmt.Println("Hello world")
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterUserV1Server(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
